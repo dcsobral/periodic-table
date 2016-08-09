cecc9d5a71881e213cab39e99ea1cfa5e6081850
Daniel Sobral <dcsobral@gmail.com>
2016-08-09 04:19:06 -0600

    Document SymbolValidator

--- src/main/java/com/dcsobral/challenges/periodictable/SymbolValidator.java
+++ src/main/java/com/dcsobral/challenges/periodictable/SymbolValidator.java
@@ -1,12 +1,20 @@
 package com.dcsobral.challenges.periodictable;
 
 /**
+ * Validates symbols for a given element.
+ *
  * @author dsobral
- * @since 8/9/16
+ * @since 8 /9/16
  */
 public class SymbolValidator {
   private final String element;
 
+
+  /**
+   * Instantiates a new Symbol validator.
+   *
+   * @param element Element which the symbols should be valid for.
+   */
   public SymbolValidator(CharSequence element) {
     this.element = element.toString().toLowerCase();
   }
@@ -19,6 +27,22 @@ public class SymbolValidator {
     return element.indexOf(Character.toLowerCase(letter)) >= 0;
   }
 
+  /**
+   * Verify that the {@code symbol} is valid for the element of this validator.
+   *
+   * <p>Valid symbols must obey the following rules:</p>
+   * <pre>
+   *   <ol>
+   *     <li>They must have exactly two letters;</li>
+   *     <li>The letters of the symbol must appear in the element name;</li>
+   *     <li>The letters of the symbol must be in the same order as in the element name;</li>
+   *     <li>If the letters in the symbol are the same, it must appear twice in the element name.</li>
+   *   </ol>
+   * </pre>
+   *
+   * @param symbol Symbol which must be valid for the element of this validator.
+   * @return true if all rules are obeyed, false otherwise.
+   */
   public boolean validate(CharSequence symbol) {
 
     if (!hasValidSize(symbol)) {
